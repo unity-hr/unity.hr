2219d07bb33686771b788d6a8bfb5efb5048e17b
Matija Marohnić <matija.marohnic@gmail.com>
2014-05-15 18:59:38 +0200

    Implement revving

--- gulpfile.js
+++ gulpfile.js
@@ -50,8 +50,10 @@ gulp.task('html', ['views', 'styles', 'scripts'], function () {
     .pipe(cssFilter)
     .pipe($.csso())
     .pipe(cssFilter.restore())
+    .pipe($.rev())
     .pipe($.useref.restore())
     .pipe($.useref())
+    .pipe($.revReplace())
     .pipe(gulp.dest('dist'))
     .pipe($.size());
 });
